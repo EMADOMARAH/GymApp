12bf0ab22521073208c73772f8bccf6cef7b616c
EMADOMARAH <emadomarah2020@gmail.com>
2021-01-25 15:50:43 +0200

    hi

---
+++ app/src/main/java/com/warbugs/gym/signinScreen.java
@@ -0,0 +1,44 @@
+package com.warbugs.gym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.warbugs.gym.Signup.signupScreen;
+
+public class signinScreen extends AppCompatActivity {
+
+    TextView signuptxt;
+    Button signinbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_screen);
+        signuptxt  =findViewById(R.id.signuptxt);
+        signinbtn = findViewById(R.id.loginbtn);
+
+        signuptxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(signinScreen.this, signupScreen.class);
+                startActivity(i);
+
+            }
+        });
+
+        signinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(signinScreen.this, "hhhhhh", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/warbugs/gym/splash/splashScreen.java
@@ -0,0 +1,28 @@
+package com.warbugs.gym.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+import com.warbugs.gym.R;
+import com.warbugs.gym.signinScreen;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splashScreen.this, signinScreen.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
