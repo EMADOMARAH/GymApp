961ecf0af31a14cd000237f61b86d5b728a13fb3
EMADOMARAH <emadomarah2020@gmail.com>
2021-01-26 19:48:50 +0200

    add bottom navigation

--- app/src/main/java/com/warbugs/gym/MainActivity.java
+++ app/src/main/java/com/warbugs/gym/MainActivity.java
@@ -3,12 +3,25 @@ package com.warbugs.gym;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+
+
+
     }
 }
